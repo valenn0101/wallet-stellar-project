24bbc645a281b2e2f33e620a2645e77347abfa42
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-17 16:12:23 -0300

    Add constats for Custom Errors
    In this file, we will store, for example, our custom errors.

---
+++ src/utils/constants.ts
@@ -0,0 +1,21 @@
+export const ERROR_MESSAGES = {
+  invalidKey: 'Invalid secret key.',
+  invalidLength: 'Secret key must be 56 characters long.',
+  invalidStart: "Secret key must start with 'S'.",
+};
+
+export const TOAST_ALERT_MESSAGE = {
+  signOut: 'Session successfully logged out',
+  notFoundAccount: 'Account not found',
+};
+
+export const ALERT_MESSAGES = {
+  alert: 'Please wait a moment! 🛑🛑',
+  copyYourKeys: 'Please copy your keys!',
+};
+
+export const CUSTOM_TOAST_STYLE = {
+  alertBackground: 'red',
+  fontColor: 'white',
+  progressBackground: 'white',
+};
