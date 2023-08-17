b46fe026b1727a1eb9ab444a7be84c6016b70a6b
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-17 15:55:28 -0300

    Add Register Functions
    The added logical methods have the functionality to generate a new key pair that will be passed to the setKeyPair component, which is responsible for setting the state of HomePage

--- src/components/Home/Register.tsx
+++ src/components/Home/Register.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Keypair } from 'stellar-sdk';
+import setKeypair from '../../utils/setKeyPair';
 
-function Register(): React.ReactElement {
+interface RegisterProps {
+  setSecret: (secret: string | null) => void;
+  setPublicKey: (publicKey: string | null) => void;
+}
+
+function Register({ setSecret, setPublicKey }: RegisterProps): React.ReactElement {
+  const handleCreateAccount = () => {
+    const pair = Keypair.random();
+    setKeypair({
+      setSecret,
+      setPublicKey,
+      pair,
+    });
+  };
+
   return (
     <div className="d-grid gap-2 mt-3">
       <p>Don&apos;t have an account? 👇👇👇</p>
-      <Button variant="secondary">Click here</Button>
+      <Button variant="secondary" onClick={handleCreateAccount}>
+        Click here
+      </Button>
     </div>
   );
 }
