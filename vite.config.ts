33f6d1be4691d6c9f13ebd2d813e2754eb757d19
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-17 15:31:22 -0300

    Add Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,19 @@ export default defineConfig({
       { find: '@/Assets', replacement: '/src/Assets' },
       { find: '@/Components', replacement: '/src/Components' },
     ],
-  }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                buffer: true
+            })
+        ]
+    }
+}
 })
