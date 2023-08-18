4fadf9fd649eef4bee8c8cbd0eb667d5acea5d3e
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-17 22:48:33 -0300

    Add HomePage e2e test
    HomePage Component:
    Verify that the "CreateKeys" and "Login" components are rendered correctly on the page.
    Check that elements related to the title, input field, "Submit" button, "Don't have an account?" link, and "Click here" button exist on the page.
    
    Login Component:
    Display an error message if the length of the secret key is less than 56 characters.
    Display an error message if the secret key does not start with 'S'.
    Submit the form with a valid secret key and verify that the error message is not displayed.
    
    Generate Keys Component:
    Generate a secret key by clicking the "Click here" button.
    Verify that the input field is not empty after generating the secret key.

---
+++ cypress/e2e/HomePage.cy.ts
@@ -0,0 +1,49 @@
+describe("HomePage Component", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:5173"); 
+    });
+    it("Should render CreateKeys and Login components", () => {
+      cy.get(".auth-form-title").should("contain", "Sign In");
+      cy.get('[placeholder="Secret Key"]').should("exist");
+      cy.get('button:contains("Submit")').should("exist");
+  
+      cy.contains("Don't have an account?").should("exist");
+      cy.get('button:contains("Click here")').should("exist");
+    });
+  });
+  describe("Login Component", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:5173"); 
+      cy.get('input[type="text"]').as("inputForKey");
+      cy.get('button:contains("Submit")').as("submitButton");
+    });
+    it("Should show a displays error toast for a secretkey lenght < 56", () => {
+      const invalidSecretKey = "invalidSecretKey";
+  
+      cy.get("@inputForKey").type(invalidSecretKey);
+      cy.get("@submitButton").click();
+  
+      cy.get(".error-toast").should("be.visible");
+      cy.contains("Invalid Key").should("be.visible");
+    });
+  
+    it("Should submits the form with a valid secret key", () => {
+      const validSecretKey =
+        "SCYV7VXVUSFMU4R4JOJL2E47UJJMK3LWL3PIZBROXP4Q3JAQVIXQFTYB";
+  
+      cy.get("@inputForKey").clear().type(validSecretKey);
+      cy.get("@submitButton").click();
+  
+      cy.get(".error-toast").should("not.exist");
+    });
+  });
+  describe("Generate Keys Component", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:5173"); 
+    });
+  
+    it("Should generate secret key when you click the button 'Click here'", () => {
+      cy.get('button:contains("Click here")').click();
+      cy.get('input[type="text"]').should("not.have.value", "");
+    });
+  });
