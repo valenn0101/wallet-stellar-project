e98bb51e55478ba5bb9a3f11436a1da7e2de1376
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-18 13:40:19 -0300

    Config Cypress Test Components

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,9 +4,17 @@ export default defineConfig({
   env: {
     baseUrl: "http://localhost:5173", // Cambia el puerto si es necesario
   },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implementa aquí los event listeners de node
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
