be4072887476050be19856b205473fd93c3a6f0a
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-18 12:05:35 -0300

    Add getPublicKey
    This module is responsible for verifying in the chosen Stellar instance whether the entered SecretKey exists; otherwise, it returns an error.

---
+++ src/utils/getPublicKey.ts
@@ -0,0 +1,16 @@
+import { Keypair, Server } from "stellar-sdk";
+
+import { TESTNET_HORIZON_URL } from "./constants";
+
+export async function getPublicKey(secretKey: string): Promise<string | any> {
+  try {
+    const keypair = Keypair.fromSecret(secretKey);
+
+    const server = new Server(TESTNET_HORIZON_URL);
+    await server.loadAccount(keypair.publicKey());
+
+    return keypair.publicKey();
+  } catch (error) {
+    return error;
+  }
+}
