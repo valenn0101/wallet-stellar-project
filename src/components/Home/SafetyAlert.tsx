c1214bd6197b88bc9b335390f6126ded931cb85e
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-18 11:17:08 -0300

    Add SafetyAlert
    The SafetyAlert component is a window that prompts the user to remember to copy their keys. It only appears when a session is started or there is an active session.

---
+++ src/components/Home/SafetyAlert.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+
+type Keypair = {
+  secretKey: string;
+  publicKey: string;
+};
+function SafetyAlert({ secretKey, publicKey }: Keypair): React.ReactElement {
+  const dispatch = useDispatch();
+
+  const backHome = () => {
+    dispatch({
+      type: "SIGN_OUT",
+    });
+  };
+  return (
+    <>
+      <h3 className="Auth-form-title">Dear User please copy your keys!</h3>
+      <div className="form-group mt-3">
+        <div className="container">
+          <h6>Your Secret Key</h6>
+          <p className="overflow-auto" style={{ maxHeight: "200px" }}>
+            {secretKey}{" "}
+          </p>
+        </div>
+
+        <div className="container">
+          <h6>Your Public Key</h6>
+          <p className="overflow-auto" style={{ maxHeight: "200px" }}>
+            {publicKey}{" "}
+          </p>
+        </div>
+      </div>
+      <div className="d-grid gap-2 mt-3">
+        <Button variant="secondary" onClick={backHome}>
+          {" "}
+          Back Home{" "}
+        </Button>
+        <Button variant="warning"> Continue </Button>
+      </div>
+    </>
+  );
+}
+
+export default SafetyAlert;
