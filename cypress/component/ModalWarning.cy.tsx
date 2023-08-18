9a8ad9bf3947f77e8f01092ae2cfc649a287ddb7
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-18 13:40:46 -0300

    Add ModalWarning test component
    Verify that the component correctly receives props when declared

---
+++ cypress/component/ModalWarning.cy.tsx
@@ -0,0 +1,21 @@
+import WarningModal from "../../src/components/WarningModal";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+describe("WarningModal Component", () => {
+  it("Should renders correctly with given props", () => {
+    const props = {
+      show: true,
+      onClose: cy.stub(),
+      tittleMessage: "Warning!",
+      message: "This is a warning message.",
+    };
+
+    cy.mount(<WarningModal {...props} />);
+
+    cy.get(".modal-warning").should("be.visible");
+    cy.get(".modal-title").should("have.text", "Warning!");
+    cy.get(".modal-body").should("have.text", "This is a warning message.");
+    cy.get(".button").contains("Close").should("be.visible");
+    cy.get(".button").contains("Continue").should("be.visible");
+  });
+});
