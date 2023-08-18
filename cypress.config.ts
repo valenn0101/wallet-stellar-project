fabbf3401f668a9451a1236e7a5cac35a13a1cbe
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-17 22:47:06 -0300

    Add New config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    baseUrl: "http://localhost:5173", // Cambia el puerto si es necesario
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
+      // implementa aquí los event listeners de node
     },
   },
 });
