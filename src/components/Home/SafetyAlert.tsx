7a0928f7daad5358a4c6cbcbf27bea9f4921bc07
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-18 13:34:03 -0300

    Add new classnames
    safety-alert-tittle, public-key and secret-key
    They were added to make them easily recognizable in an e2e test.

--- src/components/Home/SafetyAlert.tsx
+++ src/components/Home/SafetyAlert.tsx
@@ -31,28 +31,39 @@ function SafetyAlert({ secretKey, publicKey, setSecret, setPublicKey }: Keypair)
   };
   return (
     <>
-      <h3 className="Auth-form-title">Dear User please copy your keys!</h3>
+      <h3 className="auth-form-title safety-alert-title">
+        Dear User please copy your keys!
+      </h3>
       <div className="form-group mt-3">
         <div className="container">
           <h6>Your Secret Key</h6>
-          <p className="overflow-auto" style={{ maxHeight: "200px" }}>
+          <p
+            className="overflow-auto secret-key"
+            style={{ maxHeight: "200px" }}
+          >
             {secretKey}{" "}
           </p>
         </div>
 
         <div className="container">
           <h6>Your Public Key</h6>
-          <p className="overflow-auto" style={{ maxHeight: "200px" }}>
+          <p
+            className="overflow-auto public-key"
+            style={{ maxHeight: "200px" }}
+          >
             {publicKey}{" "}
           </p>
         </div>
       </div>
       <div className="d-grid gap-2 mt-3">
+        <Button variant="warning" onClick={openModal}>
+          {" "}
+          Continue{" "}
+        </Button>
         <Button variant="secondary" onClick={backHome}>
           {" "}
           Back Home{" "}
         </Button>
-        <Button variant="warning" onClick={openModal}> Continue </Button>
       </div>
 
       <WarningModal
