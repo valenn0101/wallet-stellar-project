ac5ff1c5334ef0558c5c657b097f6a9e06b6cabb
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-22 09:28:36 -0300

    fix useBalance
    Added a function named fetchBalance that holds the logic to load the account balance and update the balance state. Inside the useEffect, fetchBalance was called initially to load the initial balance. An interval was set up that calls fetchBalance every 5 seconds to keep the balance updated. A cleanup function was added to stop the interval when the component unmounts, using clearInterval

--- src/hooks/useBalance.tsx
+++ src/hooks/useBalance.tsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Server } from 'stellar-sdk';
 
 import { TESTNET_HORIZON_URL } from '../utils/constants';
@@ -6,24 +6,28 @@ import { TESTNET_HORIZON_URL } from '../utils/constants';
 type BalanceResult = string | Error;
 
 const useBalance = ({ publicKey }): BalanceResult => {
-    const [balance, setBalance] = useState<BalanceResult>(undefined);
-    const server = new Server(TESTNET_HORIZON_URL);
+  const [balance, setBalance] = useState<BalanceResult>(undefined);
+  const server = new Server(TESTNET_HORIZON_URL);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const account = await server.loadAccount(publicKey);
-                const accountBalance = account.balances[0]?.balance;
-                setBalance(accountBalance);
-            } catch (error) {
-                setBalance(new Error(error.message));
-            }
-        };
+  const fetchBalance = async () => {
+    try {
+      const account = await server.loadAccount(publicKey);
+      const accountBalance = account.balances[0]?.balance;
+      setBalance(accountBalance);
+    } catch (error) {
+      setBalance(new Error(error.message));
+    }
+  };
 
-        getData();
-    }, [publicKey]);
+  useEffect(() => {
+    fetchBalance(); 
 
-    return balance;
+    const interval = setInterval(fetchBalance, 5000); 
+
+    return () => clearInterval(interval);
+  }, [publicKey]);
+
+  return balance;
 };
 
 export default useBalance;
