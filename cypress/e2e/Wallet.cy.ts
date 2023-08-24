cc2650bd467b2eeec1d01b32c11803dad22b177c
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-24 16:02:11 -0300

    fix Wallet test
    All tests have been grouped into a single one. The 'projectUrl' variable has been set to store the project's development URL. Aliases were also added where possible, and some test case names were renamed.

--- cypress/e2e/Wallet.cy.ts
+++ cypress/e2e/Wallet.cy.ts
@@ -1,50 +1,44 @@
-describe("Login with Account", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:5173"); 
-        cy.get('input[type="text"]').as("inputForKey");
-        cy.get('button:contains("Submit")').as("submitButton");
-      });
-      it("Should login with an account and show a balance", () => {
-        const validSecretKey =
-          "SCYV7VXVUSFMU4R4JOJL2E47UJJMK3LWL3PIZBROXP4Q3JAQVIXQFTYB";
-    
-        cy.get("@inputForKey").clear().type(validSecretKey);
-        cy.get("@submitButton").click();
-    
-        cy.get(".error-toast").should("not.exist");
-        cy.get('button:contains("Continue")').click();
-        cy.get(".button-wallet").click();
-        cy.get(".public-key").should("be.visible");
-        cy.get(".balance-info").should("be.visible");
-      });
-})
+describe("Login and Account Actions", () => {
+  const projectUrl = "http://localhost:5173";
+  beforeEach(() => {
+    cy.visit(projectUrl); 
+    cy.get('input[type="text"]').as("inputForKey");
+    cy.get('button:contains("Submit")').as("submitButton");
+    cy.get('button:contains("Click here")').as('registerButton');
+    cy.get('button:contains("Click here")').click();
+    cy.get('button:contains("Continue")').as("continueButton");
+    cy.get('button:contains("Back Home")').as("backButton").click();
+  });
 
-describe('Login without Account', () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:5173"); 
-        cy.get('button:contains("Click here")').click();
-        cy.get('button:contains("Continue")').as("continueButton");
-      });
-      it("Should login and not show a balance", () => {
-        cy.get("@continueButton").click();
-        cy.get(".button-wallet").click();
-        cy.get(".title-warning").contains("Oh oh, you don't have any XLM");
-        cy.get(".info").should("be.visible");
-      })
-})
+  it("Should log in and show the user balance", () => {
+    const validSecretKey =
+      "SCYV7VXVUSFMU4R4JOJL2E47UJJMK3LWL3PIZBROXP4Q3JAQVIXQFTYB";
 
-describe('Charge account', () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:5173"); 
-        cy.get('button:contains("Click here")').click();
-        cy.get('button:contains("Continue")').click();
-        cy.get(".button-wallet").click();
-    });
-    
-    it("Should charge account", () => {
-        cy.get(".charge-btn").click();
-        cy.get(".toast-alert", { timeout: 10000 }).should("contain", "Account charged successfully!");
-        cy.reload();
-        cy.get(".balance-info").should("be.visible");
-    });
+    cy.get("@inputForKey").clear().type(validSecretKey);
+    cy.get("@submitButton").click();
+
+    cy.get(".error-toast").should("not.exist");
+    cy.get("@continueButton").click();
+    cy.get(".button-wallet").click();
+    cy.get(".public-key").should("be.visible");
+    cy.get(".balance-info").should("be.visible");
+  });
+
+  it("Should login without an account and not show a balance", () => {
+    cy.get("@registerButton").click();
+    cy.get("@continueButton").click();
+    cy.get(".button-wallet").click();
+    cy.get(".title-warning").contains("Oh oh, you don't have any XLM");
+    cy.get(".info").should("be.visible");
+  });
+
+  it("Should charge the account and show a balance", () => {
+    cy.get("@registerButton").click();
+    cy.get("@continueButton").click();
+    cy.get(".button-wallet").click();
+    cy.get(".charge-btn").click();
+    cy.get(".toast-alert", { timeout: 10000 }).should("contain", "Account charged successfully!");
+    cy.reload();
+    cy.get(".balance-info").should("be.visible");
+  });
 });
