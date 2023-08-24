c02a366c74452770b1e4d6409da8ceb08ae8935f
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-24 16:56:25 -0300

    fix accountBalance type
    useState and useEffect in the Balance component are used to manage and update the local state 'isStringBalance' based on whether 'accountBalance' is a string or not. This allows the component to render the appropriate user interface depending on the type of balance being displayed

--- src/components/Wallet/Balance/Balance.tsx
+++ src/components/Wallet/Balance/Balance.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Alert } from 'react-bootstrap';
 
 import useBalance from '../../../hooks/useBalance';
@@ -7,13 +7,19 @@ import xlmLogo from '../../../assets/xlm-logo.png';
 
 function Balance(publicKey) {
   const accountBalance = useBalance(publicKey);
+  const [isStringBalance, setIsStringBalance] = useState(accountBalance instanceof String);
+
+  useEffect(() => {
+    setIsStringBalance(typeof accountBalance === 'string');
+  }, [accountBalance]);
+
   return (
     <>
       <Row>
         <Col>
           <h3 className="text-primary">Account balance</h3>
           <div className="d-flex align-items-center">
-            { typeof accountBalance === "string"  ? (
+            { isStringBalance ? (
               <> 
                 <Alert variant='success' className='balance-info'>{accountBalance}</Alert>
                 <img src={xlmLogo} alt="XLM Logo" width="30" height="30" className="ml-2" style={{
