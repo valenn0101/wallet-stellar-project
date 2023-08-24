7dadb7b4f043fc1736b2f1ff659a35243833e24c
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-24 15:47:56 -0300

    Fix SignOut test
    The 'projectUrl' variable was set to store the project's development address. Also, 'currentTime' was modified to capture the exact time. In addition, aliases were implemented where possible.

--- cypress/e2e/SingOut.cy.ts
+++ cypress/e2e/SingOut.cy.ts
@@ -1,22 +1,22 @@
 describe('Sign Out', () => {
-    beforeEach(() => {
-      const currentTime = new Date('2023-08-22T00:00:00Z').getTime();
-      cy.clock(currentTime);
-      
-      cy.visit("http://localhost:5173"); 
-      cy.get('button:contains("Click here")').click();
-    });
-    
-    it("Should sign out after 3 minutes", () => {
-      cy.tick(190000);
-      cy.get(".auth-form-title").should("contain", "Sign In");
-    });
-
-    it("Should sign out when you click the Sign Out button", () => {
-        cy.get('button:contains("Continue")').click();
-        cy.get(".button-wallet").click();
-        cy.get(".sign-out").click();
-        cy.get(".auth-form-title").should("contain", "Sign In");
-    });
+  const projectUrl = "http://localhost:5173";
+  beforeEach(() => {
+    const currentTime = Date.now();
+    cy.clock(currentTime);
+    cy.visit(projectUrl); 
+    cy.get(".auth-form-title").should("contain", "Sign In").as("authFormTitle");
+    cy.get('button:contains("Click here")').click();
   });
-  
+
+  it("Should log out after 3 minutes of inactivity", () => {
+    cy.tick(190000);
+    cy.get("@authFormTitle")
+  });
+
+  it("Should sign out when you click the Sign Out button", () => {
+      cy.get('button:contains("Continue")').click();
+      cy.get(".button-wallet").click();
+      cy.get(".sign-out").click();
+      cy.get("@authFormTitle")
+    });
+});
