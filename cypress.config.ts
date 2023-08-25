c198ef322fc8946be17548c0fa29672d20432459
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-25 16:24:08 -0300

    Fix Cypress config
    Delete comments and change enviroment variable name

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   env: {
-    baseUrl: 'http://localhost:5173', // Cambia el puerto si es necesario
-    PROJECT_URL: 'http://localhost:5173',
+    baseUrl: 'http://localhost:5173',
   },
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implementa aquí los event listeners de node
     },
   },
 
