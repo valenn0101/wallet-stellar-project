b8c2a86b1ca0c71840a5b1495ac12bd716f56383
valenn0101 <veroncaceresvalentin@gmail.com>
2023-08-29 10:46:33 -0300

    Fix connection persistence issue on changing publicKey
    Added logic to detect changes in the publicKey parameter and disconnect the existing account stream if necessary. This ensures that only the active connection corresponds to the current public key. Introduced a currentPublicKey variable to track the current public key being streamed.

--- src/utils/streamAccounBalance.ts
+++ src/utils/streamAccounBalance.ts
@@ -5,31 +5,46 @@ const TESTNET_HORIZON = import.meta.env.VITE_TESTNET_HORIZON_URL;
 
 type BalanceCallback = (balance: number | string) => void;
 
+let currentPublicKey: string | null = null;
+let accountStream: any = null;
+
 const streamAccount = (publicKey: string, balanceCallback: BalanceCallback) => {
-  const server = new Server(TESTNET_HORIZON);
-
-  let previousBalance: number | null = null;
-
-  const accountStream = server.accounts().accountId(publicKey).stream({
-    onmessage: (account) => {
-      if ('balances' in account) {
-        const balance = account.balances.find(asset => asset.asset_type === 'native');
-        const xlmBalance = balance ? parseFloat(balance.balance) : 0;
-
-        if (xlmBalance !== previousBalance) {
-          previousBalance = xlmBalance;
-          balanceCallback(xlmBalance);
-        }
-      } else {
-        balanceCallback(ERROR_MESSAGES.invalidAccount);
-      }
-    },
-    onerror: (error) => {
-      balanceCallback(ERROR_MESSAGES.invalidAccount + error.message);
+  if (publicKey !== currentPublicKey) {
+    currentPublicKey = publicKey;
+    if (accountStream) {
+      accountStream(); // Close the previous stream
+      accountStream = null;
     }
-  });
+  }
 
-  return accountStream;
+  if (!accountStream) {
+    const server = new Server(TESTNET_HORIZON);
+
+    accountStream = server.accounts().accountId(publicKey).stream({
+      onmessage: (account) => {
+        if ('balances' in account) {
+          const balance = account.balances.find(asset => asset.asset_type === 'native');
+          const xlmBalance = balance ? parseFloat(balance.balance) : 0;
+
+          if (typeof xlmBalance === 'number') {
+            balanceCallback(xlmBalance);
+          }
+        } else {
+          balanceCallback(ERROR_MESSAGES.invalidAccount);
+        }
+      },
+      onerror: (error) => {
+        balanceCallback(ERROR_MESSAGES.invalidAccount + error.message);
+      }
+    });
+  }
+
+  return () => {
+    if (accountStream) {
+      accountStream();
+      accountStream = null;
+    }
+  };
 };
 
 export default streamAccount;
